0c08bedec63e9ed72a06a7f06cda61040da5e2f8
SathsaraniBS <sandunibbs@gmail.com>
2025-07-16 23:08:08 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-
+import React from "react";
+import{ BrowserRouter as Router} from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import WorkoutSessions from "./components/Workout";
 
 function App() {
 
   return (
-    <>
+    <Router>
+      <Navbar />
+      <Hero />
+      <WorkoutSessions/>
       
-    </>
+    </Router>
   )
 }
 
