9de66a06651356d21b8f406dbd0f5553bb8aa6cf
SathsaraniBS <sandunibbs@gmail.com>
2025-07-21 19:01:07 +0530

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,30 @@
 import React from 'react'
 
-function Navbar() {
+const Navbar = () => {
+    const [click, setClick] = useState(false);
+
+    const [button, setButton] = useState(true);
+
+    const handleClick = () => setClick(!click);
+
+    const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if(window.innerWidth <= 960) {
+            setButton(false);
+        } else {
+            setButton(true);
+        }
+    }
+
+    window.addEventListener('resize', showButton);
+
+    useEffect(() => {
+        showButton();
+    }, []);
+
+
+
   return (
     <header>
       <p>FitTrack</p>
