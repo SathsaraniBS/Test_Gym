0a51ebd6b093e850d7d6a09774c531d3d9e932c1
SathsaraniBS <sandunibbs@gmail.com>
2025-07-21 22:19:21 +0530

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,9 +2,26 @@ import React from 'react'
 
 function Hero() {
   return (
-    <div>
+    <section className="hero">
+    <div className="content">
+        <div className="title">
+            <h1>LET</h1>
+            <h1>GET</h1>
+            <h1>IN SHAPE</h1>
+        </div>
+
+        <div className="subtitle">
+            <p>Your Journey to Fitness Start Here</p>
+            <p>BSS  Creation</p>
+        </div>
+
+        <div className="buttons">
+            <button>Start Your Journey</button>
+            <button>Discover Your Plan</button>
+        </div>
       
     </div>
+    </section>
   )
 }
 
--- src/components/WorkoutSessions.jsx
+++ src/components/WorkoutSessions.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 function WorkoutSessions() {
   return (
-    <div>
+    <section className="workout-sessions">
+    <div className="wrapper">
+        <h1>TOP WORKOUT SESSION</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, doloremque.</p>
       
     </div>
+    </section>
   )
 }
 
