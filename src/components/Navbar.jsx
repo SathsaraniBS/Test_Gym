41dc126361a1c47e86d9c99f40dc7cdcdcc44899
SathsaraniBS <sandunibbs@gmail.com>
2025-07-21 19:06:40 +0530

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import { Button } from './Button';
+import './Navbar.css';
 
 const Navbar = () => {
     const [click, setClick] = useState(false);
@@ -26,11 +29,63 @@ const Navbar = () => {
 
 
   return (
-    <header>
+    /* <header>
       <p>FitTrack</p>
       
-    </header>
-  )
+    </header> */
+     <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+            <img src="./images/logo.png" alt="logo" />
+            
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click? 'fas fa-times' : 'fas fa-bars'}></i>
+          </div>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className="nav-item">
+              <Link to="/home" className="nav-links" onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/about us" className="nav-links" onClick={closeMobileMenu}>
+                About Us
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/contact us" className="nav-links" onClick={closeMobileMenu}>
+                Contact Us
+              </Link>
+            </li>
+            <li className="nav-item">
+                <Link to="/planning a trip" className="nav-links" onClick={closeMobileMenu}>
+                    Planning a Trip
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Link to="/article" className="nav-links" onClick={closeMobileMenu}>
+                    Article
+                </Link>
+            </li>
+            <li>
+                <Link to="/community and reviews" className="nav-links" onClick={closeMobileMenu}>
+                Community and Reviews</Link>
+            </li>
+
+            <li>
+                <Link to="/sign in" className="nav-links-mobile" onClick={closeMobileMenu}>
+                Sign in</Link>
+            </li> 
+          </ul>
+
+          {button && <Button buttonStyle="btn--outline">SIGN IN</Button>}
+          {button && <Button buttonStyle="btn--outline">Register</Button>}
+        </div>
+      </nav>
+    </>
+  );
 }
 
 export default Navbar
